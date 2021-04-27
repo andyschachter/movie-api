87f53257328b33c4661ab7b0d8fb9bf1a99a65af
Andy Schachter <andy.schachter@gmail.com>
2021-04-27 18:53:29 -0400

    added get request for all movies and catchall send status 404

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const movies = require('./movies')
+const { request } = require('express')
 
 const app = express()
+
+app.get('/movies', (request, response) => {
+  return response.send(movies)
+})
+
+app.all('*', (request, response) => {
+  return response.sendStatus(404)
+})
 
 app.listen(1343, () => {
   console.log('listening on port 1343...') // eslint-disable-line no-console
